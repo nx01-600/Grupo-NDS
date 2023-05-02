eb5ecd0a3edc9c300ae658025732ca863b41f5ec
Figue4870 <131775845+Figue4870@users.noreply.github.com>
2023-05-02 08:12:24 -0500

    actualización_multiplicación.py
    Se creo la función que se llama para poder multiplicar.

--- math_game.py
+++ math_game.py
@@ -7,3 +7,8 @@ def sub(num_1, num_2):
     result = num_1 - num_2
     print(f'{num_1} - {num_2} is equal to {result}')
     return result
+    
+def mul(num_1, num_2):
+    result = num_1 * num_2
+    print(f'{num_1} * {num_2} is equal to {result}')
+    return result
