7e395ca51d6f081b97ef08e9fc64070554c04680
David Hernández <107824698+Tr7cer@users.noreply.github.com>
2023-05-02 08:19:09 -0500

    actualizacion_division.py
    Se agrega la parte que llama a la funcion de division para asi agregar la función de dividir.
    Tambien se agrega parte de texto al enunciano del primer print siendo el de la división y algo que falto en la actualización de la multiplicación.

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ def game():
     score = 0
 
     while True:
-        print('======== Menu ========\n1. Add\n2. Substraction')
+        print('======== Menu ========\n1. Add\n2. Substraction\n3. Multiplication\n4. Division')
         operation = int(input('Choice an operation:'))
         num_1 = int(input('Enter first number: '))
         num_2 = int(input('Enter second number: '))
@@ -15,6 +15,8 @@ def game():
             result = sub(num_1,num_2)
         elif operation == 3:
             result = mul(num_1,num_2)
+        elif operation == 4:
+            result = div(num_1,num_2)
         if result == answer:
             score += 1
             print('Correct!!')
