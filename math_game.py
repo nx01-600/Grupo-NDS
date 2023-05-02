5a1d2b766c7021d1af463ac491cbf459bbb1556c
David Hernández <107824698+Tr7cer@users.noreply.github.com>
2023-05-02 08:20:34 -0500

    actualizacion_division.py
    Se agrega la función que divide los 2 números.

--- math_game.py
+++ math_game.py
@@ -12,3 +12,8 @@ def mul(num_1, num_2):
     result = num_1 * num_2
     print(f'{num_1} * {num_2} is equal to {result}')
     return result
+
+def div(num_1, num_2):
+    result = num_1 / num_2
+    print(f'{num_1} / {num_2} is equal to {result}')
+    return result
